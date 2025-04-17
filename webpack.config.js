9fcaa7f428d5eff6ec088cc731d41798d51fa323
soumyamehta210 <soumya.mehta@marutitech.com>
2025-04-17 19:26:17 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  mode: "production", // or 'development' for local dev
+};
